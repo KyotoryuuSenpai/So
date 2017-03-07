87a0981b4f0694b714b881655524629dbf57c450
javimillan6 <millanjavi.4.1112@gmail.com>
2017-03-07 15:40:36 +0100

    Prueba

--- app/src/main/java/com/example/kyotoryuu/so/Animacions.java
+++ app/src/main/java/com/example/kyotoryuu/so/Animacions.java
@@ -16,6 +16,7 @@ public class Animacions extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animacions);
 
+        //Identificacio buttons
         buttonInterpolacio = (Button)findViewById(R.id.buttonInterpolacio);
         buttonFotogrames = (Button)findViewById(R.id.buttonFotogrames);
         buttonEnrere = (Button)findViewById(R.id.buttonEnrere);
