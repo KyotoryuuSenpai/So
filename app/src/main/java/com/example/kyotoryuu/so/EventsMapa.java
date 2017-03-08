fc662ca9296074069e5801ac3ed56e8fd1a3055b
KyotoryuuSenpai <sergio_plaza_4@hotmail.com>
2017-03-08 12:43:53 +0100

    Geolocalizacion hecha pero falta 1 cosa.

--- app/src/main/java/com/example/kyotoryuu/so/EventsMapa.java
+++ app/src/main/java/com/example/kyotoryuu/so/EventsMapa.java
@@ -6,17 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class EventsMapa extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mapa;
     Button buttonEnrere;
     TextView textInfo;
+    Marker marker;
+    MarkerOptions options;
+    CameraUpdate update;
     double latitude = 0;
     double longitude = 0;
     @Override
@@ -42,14 +50,30 @@ public class EventsMapa extends AppCompatActivity implements OnMapReadyCallback 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mapa = googleMap;
+        LatLng latlng = new LatLng(40, -3);
+        options = new MarkerOptions().position(latlng);
+        marker = mapa.addMarker(options);
+        update = CameraUpdateFactory.newLatLngZoom(latlng, 5);
+        mapa.animateCamera(update);
         mapa.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng point) {
                 latitude = point.latitude;
                 longitude = point.longitude;
-                textInfo.setText(latitude+" - "+longitude);
+                marker.remove();
+                options.position(point);
+                marker = mapa.addMarker(options);
+                String posicion = "Latitud: "+latitude+"\n Longitud: "+longitude;
+                setText(posicion);
+                marker.setTitle(posicion);
+                update = CameraUpdateFactory.newLatLngZoom(point, 5);
+                mapa.animateCamera(update);
             }
         });
+    }
+
+    public void setText(String texto){
+        Toast.makeText(this, texto, Toast.LENGTH_SHORT).show();
     }
 }
 
